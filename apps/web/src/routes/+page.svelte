b0b16a97089eab577b06b6d6ca0d65534a44bfcd
Asandei Stefan <asandei.stefanel@gmail.com>
2024-12-30 11:53:29 +0200

    svelte 5

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -2,6 +2,6 @@
 	import { MyCounterButton } from '@repo/ui';
 </script>
 
-<h1>Web</h1>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <MyCounterButton />
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
