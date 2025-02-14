ddf7b91dab913baa7cbaf164acb8953f6ef45758
Asandei Stefan <asandei.stefanel@gmail.com>
2025-02-14 17:45:10 +0200

    prepare for netlify deployment

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { fileURLToPath } from 'node:url';
 import { createJiti } from 'jiti';
@@ -16,7 +16,14 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			// if true, will create a Netlify Edge Function rather than using standard Node-based functions
+			edge: false,
+
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app.
+			split: false
+		}),
 		alias: {
 			$base: '.',
 			'@/*': './src/lib/*'
