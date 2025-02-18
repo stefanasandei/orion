947a5a0800b99800c803c000496df3aef3332eb6
Asandei Stefan <asandei.stefanel@gmail.com>
2025-02-18 20:34:38 +0200

    base for note page

---
+++ apps/web/src/routes/(dashboard)/projects/doc/[noteId]/+page.server.ts
@@ -0,0 +1,30 @@
+import { redirect, type RequestEvent } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+import { authMiddleware } from "@/utils/auth-middleware.js";
+import { createCaller } from '@repo/api';
+import type { CtxRequestEvent } from '@repo/core';
+
+export const load: PageServerLoad = (async (event: RequestEvent) => {
+    // general auth check
+    const redirectUrl = authMiddleware(event);
+    if (redirectUrl !== undefined) {
+        redirect(302, redirectUrl);
+    }
+
+    // check if project is owned by user & get its data
+    const noteId = parseInt(event.params.noteId!);
+
+    // const caller = createCaller({ event: event as CtxRequestEvent });
+    // const { project, noteTree } = await caller.project.get({ id: projectId });
+
+    // if (project === undefined) {
+    //     redirect(302, "/");
+    // }
+
+    return {
+        user: event.locals!,
+        noteId
+        // project,
+        // noteTree
+    };
+}) satisfies PageServerLoad;
---
+++ apps/web/src/routes/(dashboard)/projects/doc/[noteId]/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import DashboardShell from '@/components/dashboard/shell.svelte';
+	import type { UserLocals } from '@repo/core';
+	import { t } from '@/utils/i18n/translations';
+	import type { Note, Project } from '@repo/db';
+
+	interface Props {
+		user: UserLocals;
+		noteId: number;
+	}
+
+	const { data }: { data: Props } = $props();
+	const { user } = data;
+
+	// makes navigating to another slug route reactive
+	const note = $derived(data.noteId);
+</script>
+
+<!-- keep state also in url -->
+<!-- but keep track of opened tabs in local storage, btw filetree on the left (closeable) -->
+
+<DashboardShell pageName={'Document'} {user}>
+	<p>{note}</p>
+</DashboardShell>
